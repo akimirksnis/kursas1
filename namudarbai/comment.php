fa45f737a2d7869427fa5003832a8b8e38c83b7f
akimirksnis <waitkeviciute@gmail.com>
2019-09-07 15:18:03 +0300

    komentaru budai

---
+++ namudarbai/comment.php
@@ -0,0 +1,27 @@
+  <?php
+
+
+    /*
+    komentaras gali buti
+    per kelias
+    eilutes
+    */
+
+    //komentaras per viena eilute
+
+    #komentaras skirtas piesimui
+
+    ##############
+    # 2019 09 07 #
+    #  Vilma     #
+    ##############
+
+
+
+    echo "Mano vardas";
+    echo "Vilma";
+
+
+    ?>
+
+
